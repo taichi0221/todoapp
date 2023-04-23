634d09e6322681064a1a46464ba356f0404e0f4f
taichi0221 <127753483+taichi0221@users.noreply.github.com>
2023-04-23 13:25:24 +0900

    useState

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import TodoList from "./TodoList";
 
 function App(){
+  const [todos, setTodos] = useState()  //useState 変数の管理 無駄な再レンダリングを防ぐ
   return (
   <div>
     <TodoList />
+    <input type="text" />
+    <button>タスクを追加</button>
+    <button>完了したタスクの削除</button>
+    <div>残りのタスク:0</div>
   </div>
   );
 }
